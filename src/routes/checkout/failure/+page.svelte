613d55be20b65cce6b3f06edc4b51f2698c1762d
WKogut98 <wojciech.kogut7@gmail.com>
2024-12-02 20:01:48 +0100

    stripe integration

---
+++ src/routes/checkout/failure/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Button from "$components/Button.svelte";
+import HeroSection from "$components/HeroSection.svelte";
+
+</script>
+<HeroSection>
+    <h1>SOMETHING WENT WRONG</h1>
+    <h1 class="mb-l">:-&lpar;</h1>
+    <Button class="mb-s">Try again</Button>
+    <p class="light-grey mb-s"><a href="/">Back to the homepage</a></p>
+</HeroSection>
+<style>
+    a{
+        text-decoration: underline;
+    }
+</style>
---
+++ src/routes/checkout/success/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import HeroSection from "$components/HeroSection.svelte";
+
+</script>
+<HeroSection>
+    <h1>THANK YOU FOR</h1>
+    <h1 class="mb-l">PURCHASING</h1>
+    <p class="light-grey mb-s">Check your email for confirmation</p>
+    <p class="light-grey mb-s"><a href="/">Back to the homepage</a></p>
+</HeroSection>
+<style>
+    a{
+        text-decoration: underline;
+    }
+</style>
